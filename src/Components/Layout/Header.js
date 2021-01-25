93135ad7cbf4c93992d59a8e5c956b21b6113f24
Harshal Dhake <harshaldhake123@gmail.com>
2021-01-25 15:02:50 +0530

    Homepage Design

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,40 +1,41 @@
 import React, {Component} from "react";
+import AppLogo from "./AppLogo.png";
 
 class Header extends Component {
     render() {
 
-        const userIsAuthenticated = (
-            <div className={"collapse navbar-collapse"} id="mobile-nav">
-                <ul className={"navbar-nav mr-auto"}>
-                    <li className={"nav-item"}>
-                        Dashboard
-                    </li>
-                </ul>
-                <ul className={"navbar-nav ml-auto"}>
-                    <li className={"nav-item"}>
-                        <i className="fas fa-user-circle mr-1"/>USER's Name
-                    </li>
-                    <li className={"nav-item"}>
-                        Logout
-                    </li>
-                </ul>
-            </div>);
-
-        const userNotAuthenticated = (
-            <div className={"collapse navbar-collapse"} id="mobile-nav">
-                <ul className={"navbar-nav ml-auto"}>
-                    <li className={"nav-item"}>
-                        Sign Up
-                    </li>
-                    <li className={"nav-item"}>
-                        Login
-                    </li>
-                </ul>
-            </div>);
+        // const userIsAuthenticated = (
+        //     <div className={"collapse navbar-collapse"} id="mobile-nav">
+        //         <ul className={"navbar-nav mr-auto"}>
+        //             <li className={"nav-item"}>
+        //                 Dashboard
+        //             </li>
+        //         </ul>
+        //         <ul className={"navbar-nav ml-auto"}>
+        //             <li className={"nav-item"}>
+        //                 <i className="fas fa-user-circle mr-1"/>USER's Name
+        //             </li>
+        //             <li className={"nav-item"}>
+        //                 Logout
+        //             </li>
+        //         </ul>
+        //     </div>);
+        //
+        // const userNotAuthenticated = (
+        //     <div className={"collapse navbar-collapse"} id="mobile-nav">
+        //         <ul className={"navbar-nav ml-auto"}>
+        //             <li className={"nav-item"}>
+        //                 Sign Up
+        //             </li>
+        //             <li className={"nav-item"}>
+        //                 Login
+        //             </li>
+        //         </ul>
+        //     </div>);
         return (
             <div>
-                <nav className={"navbar navbar-expand-sm navbar-dark navbar-bg-custom mb-4 navbar-sticky"}>
-                    <a className={"navbar-brand"}>BBank</a>
+                <nav className={"navbar navbar-expand-sm navbar-dark navbar-bg-custom navbar-sticky"}>
+                    <a className={"navbar-brand"}> <img src={AppLogo} alt="Logo" id={"navbar-brand-logo"}/> BBank</a>
                     {/*<div className={"container"}>*/}
                     {/*    Blood Bank Management System*/}
 
@@ -57,7 +58,8 @@ class Header extends Component {
                         </ul>
                     </div>
                 </nav>
-            </div>);
+            </div>
+        );
     }
 
 }
--- src/Components/Layout/Landing.js
+++ src/Components/Layout/Landing.js
@@ -3,9 +3,21 @@ import React, {Component} from "react";
 class Landing extends Component {
     render() {
         return (
-            <div className="landing">
+            <section className={"colored-section"} id="title">
+                <div className={"container-fluid"}>
+                    <div className="landing row">
+                        <div className={"col-lg-6 col-md-12"}>
+                            <h1 className={"big-heading"}>Give someone the gift of life.</h1>
+                        </div>
 
-            </div>
+                        <div className={"container-search-bank-form col-lg-6 col-md-12"}>
+                            <h3 className={"h3-temp"}>Find Nearby Blood Banks</h3>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }

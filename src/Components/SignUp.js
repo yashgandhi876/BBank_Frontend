53330f5c5e1449eac735243b15fef53253c083b5
Yash Gandhi <yashgandhi2020@gmail.com>
2021-01-26 12:53:30 +0530

    Added user data input with css

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -17,13 +17,27 @@ class SignUp extends Component {
                 country: "",
                 state: "",
                 city: "",
+                address:"",
+            },
+            user:{
+                name: "",
+                email: "",
+                mobileNumber: "",
+                gender:"",
+                blood_group:"",
+                age:"",
+                country: "",
+                state: "",
+                city: "",
+                address:"",
             },
             countryCode:0,
             stateCode:0,
             cityCode:0
 
          };
-        this.submitFormAction = this.submitFormAction.bind(this);
+        this.submitBloodBankForm = this.submitBloodBankForm.bind(this);
+        this.submitUserForm = this.submitUserForm.bind(this);
         this.handleChangeCat = this.handleChangeCat.bind(this);
         this.handleCountry = this.handleCountry.bind(this);
         this.handleState = this.handleState.bind(this);
@@ -38,7 +52,7 @@ class SignUp extends Component {
     }
 
     handleCountry(e){
-        this.setState({...this.state, bbank:{...this.state.bbank,country:e.target.value}, countryCode:e.target.selectedIndex });
+        this.setState({...this.state, bbank:{...this.state.bbank,country:e.target.value}, countryCode:101 });
 
     }
 
@@ -51,6 +65,7 @@ class SignUp extends Component {
     }
 
     listState(countryCode){
+        console.log(countryCode);
         let newStateArr =  StateData.states.filter(state=>(state.country_id == countryCode));
 
         return (
@@ -76,8 +91,12 @@ class SignUp extends Component {
         );
     }
 
-    submitFormAction(){
+    submitBloodBankForm(){
         // add code
+    }
+
+    submitUserForm(){
+        //add code
     }
 
     checkCat(value){
@@ -88,6 +107,7 @@ class SignUp extends Component {
                     <br /><input type="email" className="inputBox" value={this.state.bbank.email} onChange={(e)=>{this.setState({...this.state, bbank:{...this.state.bbank,email:e.target.value} })}} placeholder="Email" required/>
                     <br /><input type="tel" className="inputBox" value={this.state.bbank.mobileNumber} onChange={(e)=>{this.setState({...this.state, bbank:{...this.state.bbank,mobileNumber:e.target.value} })}} placeholder="Phone Number" required/>
                     <br /><input type="text" className="inputBox" value={this.state.bbank.stocks} onChange={(e)=>{this.setState({...this.state, bbank:{...this.state.bbank,stocks:e.target.value} })}} placeholder="Stocks" required/>
+                    <br /><input type="text" className="inputBox" value={this.state.bbank.address} onChange={(e)=>{this.setState({...this.state, bbank:{...this.state.bbank,address:e.target.value} })}} placeholder="Address" required/>
                     <br /><select className="locationdropdown" name="Country"  onChange={this.handleCountry}id="" required>
                         <option value="">Select Country</option>
                         {
@@ -98,6 +118,7 @@ class SignUp extends Component {
                     </select>
                     <br />
                     {
+
                         this.listState(this.state.countryCode)
                     }
                     <br />
@@ -105,19 +126,54 @@ class SignUp extends Component {
                         this.listCity(this.state.stateCode)
                     }
                     <br/>
-                    <input onClick="submitFormAction" className="submitbtn" type="submit"/>
+                    <button onClick={this.submitBloodBankForm} className="submitbtn" type="submit">Sign Up</button>
                 </div>
             );
-        else if( value === "Donor")
+
+        else if( value === "User")
             return (
                 <div>
-                    In Donor
-                </div>
-            );
-        else if( value === "Patient")
-            return (
-                <div>
-                    In Patient
+                    <input type="text" className="inputBox" value={this.state.user.name} onChange={(e)=>{this.setState({...this.state, user:{...this.state.user,name:e.target.value} })}} placeholder="User Name" required/>
+                    <br /><input type="email" className="inputBox" value={this.state.user.email} onChange={(e)=>{this.setState({...this.state, user:{...this.state.user,email:e.target.value} })}} placeholder="Email" required/>
+                    <br /><input type="tel" className="inputBox" value={this.state.user.mobileNumber} onChange={(e)=>{this.setState({...this.state, user:{...this.state.user,mobileNumber:e.target.value} })}} placeholder="Phone Number" required/>
+                    <br /><input type="number" className="inputBox" value={this.state.user.age} onChange={(e)=>{this.setState({...this.state, user:{...this.state.user,age:e.target.value} })}} placeholder="Age" required/>
+                    <br/> <select className="locationdropdown" name="gender" id="">
+                        <option value="Gender">gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="other">other</option>
+                    </select>
+                    <br/> <select className="locationdropdown" name="bloodgroup" id="">
+                        <option value="Blood Group">Blood Group</option>
+                        <option value="A+">A+</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B-">B-</option>
+                        <option value="O+">O+</option>
+                        <option value="O-">O-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
+                    </select>
+                    <br /><input type="text" className="inputBox" value={this.state.user.address} onChange={(e)=>{this.setState({...this.state, user:{...this.state.user,address:e.target.value} })}} placeholder="Address" required/>
+                    <br /><select className="locationdropdown" name="Country"  onChange={this.handleCountry}id="" required>
+                        <option value="">Select Country</option>
+                        {
+                            CountryData.countries.map(country =>
+                                 <option key={country.id} countryCode={country.id} value={country.name}> {country.name}</option>
+                            )
+                        }
+                    </select>
+                    <br />
+                    {
+
+                        this.listState(this.state.countryCode)
+                    }
+                    <br />
+                    {
+                        this.listCity(this.state.stateCode)
+                    }
+                    <br/>
+                    <button onClick={this.submitUserForm} className="submitbtn" type="submit">Sign Up</button>
                 </div>
             );
     }
@@ -129,8 +185,7 @@ class SignUp extends Component {
                 <select className="catigoresdropdown" name="catigores" onChange={this.handleChangeCat} id="">
                     <option value="None">catigores</option>
                     <option value="BloodBank">Blood Bank</option>
-                    <option value="Donor">Donor</option>
-                    <option value="Patient">Patient</option>
+                    <option value="User">User</option>
                 </select>
                 {
                     this.checkCat(this.state.selectCat)

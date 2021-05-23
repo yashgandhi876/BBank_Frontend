f00450873b8fe89d4dda4d4ab0991e5c3100ac10
Yash <yashgandhi2020@gmail.com>
2021-05-23 16:16:23 +0530

    added API integration for login and signup

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,46 +1,62 @@
-import React, { Component } from 'react';
-import "./SignUp.css"
+import React, { useState } from "react";
+import axios from "axios";
+import "./SignUp.css";
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectCat:"None",
-            email:"",
-            password:""
+function Login() {
+	const [selectCat, setSelectCat] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleChangeCat(e) {
+		setSelectCat(e.target.value);
+	}
+
+	async function submitUserForm() {
+		// console.log(selectCat + " " + email + "  " + password);
+        const cred = {
+            selectCat, email, password
         }
+        const data = await axios.post("http://localhost:5000/auth/login", cred);
+        console.log("data: " + data);
+	}
 
-        this.handleChangeCat = this.handleChangeCat.bind(this);
-    }
-
-
-
-    handleChangeCat(e){
-        this.setState({
-            selectCat:e.target.value
-        })
-    }
-
-    submitUserForm(){}
-
-
-    render() {
-        return (
-            <div className="LoginForm">
-                <select className="catigoresdropdown" name="catigores" onChange={this.handleChangeCat} id="">
-                    <option value="None">catigores</option>
-                    <option value="BloodBank">Blood Bank</option>
-                    <option value="User">User</option>
-                </select>
-                <div className="loginform">
-                    <input type="email" className="inputBox" value={this.state.email} onChange={(e)=>{this.setState({...this.state, email:e.target.value} )}} placeholder="Email" required/>
-                    <br /><input type="password" className="inputBox" value={this.state.password} onChange={(e)=>{this.setState({...this.state,password:e.target.value} )}} placeholder="password" required/>
-                    <br />
-                    <button onClick={this.submitUserForm} className="submitbtn" type="submit">Login</button>
-                </div>
-            </div>
-         );
-    }
+	return (
+		<div className="LoginForm">
+			<select className="catigoresdropdown" name="catigores" onChange={handleChangeCat} id="">
+				<option value="None">categories</option>
+				<option value="BloodBank">Blood Bank</option>
+				<option value="User">User</option>
+			</select>
+			<div className="loginform">
+				<input
+					type="email"
+					className="inputBox"
+					value={email}
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+					placeholder="Email"
+                    autoComplete='true'
+					required
+				/>
+				<br />
+				<input
+					type="password"
+					className="inputBox"
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+					placeholder="password"
+					required
+				/>
+				<br />
+				<button onClick={submitUserForm} className="submitbtn" type="submit">
+					Login
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
